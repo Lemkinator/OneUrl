6f86f248e70462b4dc23fc40f6054dfc9d96269f
Leonard <leo@leonard-lemke.com>
2023-10-09 23:30:47 +0200

    minor fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "de.lemke.oneurl"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         kapt {
             arguments {
