fd449108b1af34ba76d69a8491ef0d0638ea5178
Leonard <leo@leonard-lemke.com>
2023-11-06 20:16:54 +0100

    fix oobe screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "de.lemke.oneurl"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
 
         kapt {
             arguments {
