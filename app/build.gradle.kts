c1945a4ca97bcceb458edf2726526dedc2dc2a3f
Leonard <leo@leonard-lemke.com>
2024-01-30 16:15:28 +0100

    release update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 8
-        versionName = "1.0.7"
+        versionName = "1.1.0"
 
         kapt {
             arguments {
