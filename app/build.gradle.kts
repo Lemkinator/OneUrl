80b0a8b80cc303cfbe836bd758df84795aa71628
Leonard <leo@leonard-lemke.com>
2025-02-16 19:33:32 +0100

    release update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "de.lemke.oneurl"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "1.5.7"
+        versionCode = 32
+        versionName = "1.5.8"
         ksp { arg("room.schemaLocation", "$projectDir/schemas") }
     }
 
