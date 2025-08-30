3824048f1ad1c723d5911c279cb44643c1df0f70
Leonard <leo@leonard-lemke.com>
2025-08-30 16:17:14 +0200

    Update common-utils dependency to 0.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,7 @@ secrets {
 }
 
 dependencies {
-    implementation("io.github.lemkinator:common-utils:0.9.0")
+    implementation("io.github.lemkinator:common-utils:0.9.1")
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.github.skydoves:bundler:1.0.4")
     implementation("androidx.datastore:datastore-preferences:1.1.7")
