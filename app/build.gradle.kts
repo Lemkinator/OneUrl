66ebe20c26e9ee698bcf16023635aceeb27f6ebe
Leonard <leo@leonard-lemke.com>
2026-09-26 00:08:11 +0200

    chore: drop a comment that justifies a dependency
    The owner's comment rule allows comments only for undocumented external behavior.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -182,9 +182,6 @@ dependencies {
     implementation(libs.oneui.icons)
     implementation(libs.common.utils)
     implementation(libs.bundler)
-    // Pins kotlinx-coroutines-core for main/androidTest classpath parity - do not remove without
-    // re-checking dependencyInsight on both debugRuntimeClasspath and debugAndroidTestRuntimeClasspath
-    // (see commit 7e1ebd6).
     implementation(libs.coroutines.android)
     implementation(libs.volley)
     implementation(libs.bundles.room)
